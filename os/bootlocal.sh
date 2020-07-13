e8e9eeb3e940e366de0aae9961a0b5b2862b1812
Arthur de Freitas <42586099+arthurfreitas96@users.noreply.github.com>
2020-07-13 19:46:13 -0300

    Update bootlocal.sh

--- os/bootlocal.sh
+++ os/bootlocal.sh
@@ -2,8 +2,6 @@
 # put other system startup commands here
 #turn off screen after one minute of inactivity
 echo -e '\033[9;1]'
-#set baud rates for transmission
-sysctl -p /etc/sysctl.conf
 #define static ip
 opt/eth0.sh
 #start dbus to use rygel
@@ -11,6 +9,5 @@ sudo /usr/local/etc/init.d/dbus start
 #to use cron, uncomment if you need it
 #crond -L /dev/null 2>&1
 #set your path to transmission-daemon configuration
-#mine is /home/tc/apps/trans/config
 transmission-daemon -g /mnt/sda1/transmission &
 rygel --config=home/tc/.config/rygel.conf &
